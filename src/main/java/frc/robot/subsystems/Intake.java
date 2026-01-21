34a8c5a4ff67615a27cc8f167792c1d49f3b66ab
Liam Wilmot <114833225+itsameLuigi64@users.noreply.github.com>
2026-01-20 21:42:32 -0500

    Start of an intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CanIdsOtherThanDrive;
+import frc.robot.Constants.MotorConstants;
+
+public class Intake extends SubsystemBase {
+  private final SparkMax m_intake; //NEO 550
+
+  private static SparkMaxConfig m_intakeConfig = new SparkMaxConfig();
+
+  static {
+    m_intakeConfig.idleMode(IdleMode.kBrake);
+    m_intakeConfig.smartCurrentLimit(MotorConstants.kNeo550SmartCurrentLimit);
+  }
+
+  /** Creates a new Intake. */
+  public Intake() {
+    m_intake = new SparkMax(CanIdsOtherThanDrive.kIntakeId, SparkMax.MotorType.kBrushless);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
