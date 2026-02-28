0a97a63a4bdc98d718e02034ac1ceaa85f5e583b
Mplgamer23 <natthews39@gmail.com>
2026-02-28 09:56:08 -0500

    fixed stuff

--- src/main/java/frc/robot/subsystems/shooter/BeltNado.java
+++ src/main/java/frc/robot/subsystems/shooter/BeltNado.java
@@ -4,14 +4,43 @@
 
 package frc.robot.subsystems.shooter;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.MotorConstants;
 
 public class BeltNado extends SubsystemBase {
   /** Creates a new BeltNado. */
-  public BeltNado() {}
+  private SparkMax m_Motor;
+  private static SparkMaxConfig m_MotorConfig= new SparkMaxConfig();
+  static{
+  m_MotorConfig
+  .smartCurrentLimit(MotorConstants.kNeoSmartCurrentLimit)
+  .idleMode(IdleMode.kCoast);
+}
 
-  @Override
-  public void periodic() {
+public BeltNado() {
+  m_Motor = new SparkMax(Constants.CanIdsOtherThanDrive.kBeltnadoMotorId, MotorType.kBrushless);
+  m_Motor.configure(m_MotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+}
+
+private void setMotorSpeed(double speed) {
+  m_Motor.set(speed);
+}
+
+private void stopMotor() {
+  m_Motor.set(0);
+}
+
+@Override
+public void periodic() {
     // This method will be called once per scheduler run
   }
 }
